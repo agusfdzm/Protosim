2ad7c765996ace9f5f97a15ed7094a260d35fb5b
agusfdzm <129664400+agusfdzm@users.noreply.github.com>
2025-09-20 23:58:22 +0200

    Main

--- protosim/src/main/java/com/agus/protosim/App.java
+++ protosim/src/main/java/com/agus/protosim/App.java
@@ -1,14 +1,11 @@
+// main
+
 package com.agus.protosim;
 
-import com.agus.protosim.tcp.TCPSimulator;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("=== PROTOTIPO TCP ===");
-        System.out.println("Desarrollado por: Agustín");
-        System.out.println("Versión: 1.0 - Prototipo");
-        System.out.println();
-        
-        TCPSimulator.main(args);
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
